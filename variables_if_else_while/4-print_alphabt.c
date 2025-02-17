81e5b8000e56d5eb9de800e4e0639df01ed58842
kahlidfma5 <kahlidfma5@hotmail.com>
2025-02-17 18:20:32 +0000

    print_alphabt

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Description: print all aplhabet letters
- *
+ * Description: print all aplhabet letters except q and e
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char
-ch = 'a';
-while (ch <= 'z')
-if (q , e)
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
 {
-putchar (ch);
-ch++;
+if (letter != 'e' && letter != 'q')
+putchar(letter);
 }
 putchar('\n');
 return (0);
