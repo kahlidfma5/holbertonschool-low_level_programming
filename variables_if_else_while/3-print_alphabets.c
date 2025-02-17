d3595a9cbd3a88a0f587735b8a5c24fb76775b52
kahlidfma5 <kahlidfma5@hotmail.com>
2025-02-17 17:47:26 +0000

    print_alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char
+ch = 'a';
+while (ch <= 'z')
+{
+putchar (ch);
+ch++;
+}
+CH = 'A';
+while (CH <= 'Z')
+{
+putchar (CH);
+CH++;
+}
+putchar('\n');
+return (0);
+}
