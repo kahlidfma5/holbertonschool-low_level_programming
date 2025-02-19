9753190dbd41ac1e76ca6d33c4875dbfd2cdaa74
kahlidfma5 <kahlidfma5@hotmail.com>
2025-02-19 17:16:15 +0000

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,13 @@
 #include "main.h"
-
 /**
 * print_alphabet - check the code
 * print alphabet void method
 * Return: nothing.
 */
-
 void print_alphabet(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-		_putchar(ch);
-		_putchar('\n');
-	
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+_putchar(ch);
+_putchar('\n');
 }
