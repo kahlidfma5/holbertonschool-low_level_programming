cbf1fb015f62ce6b5be1b5cc2956ec6cd2dbfb7a
kahlidfma5 <kahlidfma5@hotmail.com>
2025-02-19 16:59:23 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* print_alphabet - check the code
+* print alphabet void method
+* Return: nothing.
+*/
+void print_alphabet(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+_putchar(ch);
+-putchar('\n');
+}
+
