6e9a0a4f20368e34c3347f6d6143ac78e6585e01
kahlidfma5 <kahlidfma5@hotmail.com>
2025-02-24 17:44:20 +0000

    add file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
+#include <stddef.h>
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
